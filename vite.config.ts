f150d729acaa1a549f13f7e586705b163cb71aab
Mater <bxh8640@gmail.com>
2023-02-08 18:44:13 +0800

    fix: code generate

--- vite.config.ts
+++ vite.config.ts
@@ -1,93 +1,54 @@
 import { defineConfig, Plugin } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const VIRTUAL_MODULE_ID_1 = 'VIRTUAL_MODULE_ID_1'
-const VIRTUAL_MODULE_ID_2 = 'VIRTUAL_MODULE_ID_2'
-const VIRTUAL_MODULE_ID_3 = 'VIRTUAL_MODULE_ID_3'
-const VIRTUAL_MODULE_ID_4 = 'VIRTUAL_MODULE_ID_4'
-const VIRTUAL_MODULE_ID_5 = 'VIRTUAL_MODULE_ID_5'
+const VIRTUAL_MODULE_IDS = new Array(5)
+  .fill('')
+  .map((v, k) => `VIRTUAL_MODULE_ID_${k}`)
 
-const code = {
-  [VIRTUAL_MODULE_ID_1]:
-    'export default function(){ console.log("VIRTUAL_MODULE_ID_1") }',
-  [VIRTUAL_MODULE_ID_2]:
-    'export default function(){ console.log("VIRTUAL_MODULE_ID_2") }',
-  [VIRTUAL_MODULE_ID_3]:
-    'export default function(){ console.log("VIRTUAL_MODULE_ID_3") }',
-  [VIRTUAL_MODULE_ID_4]:
-    'export default function(){ console.log("VIRTUAL_MODULE_ID_4") }',
-  [VIRTUAL_MODULE_ID_5]:
-    'export default function(){ console.log("VIRTUAL_MODULE_ID_5") }'
-}
+const code = Object.fromEntries(
+  VIRTUAL_MODULE_IDS.map(v => [
+    v,
+    `export default function() { console.log("${v}") }`
+  ])
+)
 
-function ViteAbcPlugin(): Plugin {
+function ViteVuePrePlugin(): Plugin {
   return {
-    name: 'vite-abc-plugin',
+    name: 'vite-vue-pre-plugin',
 
     handleHotUpdate(ctx) {
-      const module1 = ctx.server.moduleGraph.getModuleById(VIRTUAL_MODULE_ID_1)
-      const module2 = ctx.server.moduleGraph.getModuleById(VIRTUAL_MODULE_ID_2)
-      const module3 = ctx.server.moduleGraph.getModuleById(VIRTUAL_MODULE_ID_3)
-      const module4 = ctx.server.moduleGraph.getModuleById(VIRTUAL_MODULE_ID_4)
-      const module5 = ctx.server.moduleGraph.getModuleById(VIRTUAL_MODULE_ID_5)
-      // ctx.server.reloadModule('name', [module1, module2, module3, module4, module5])
-      module1 && ctx.server.reloadModule(module1)
-      module2 && ctx.server.reloadModule(module2)
-      module3 && ctx.server.reloadModule(module3)
-      module4 && ctx.server.reloadModule(module4)
-      module5 && ctx.server.reloadModule(module5)
+      VIRTUAL_MODULE_IDS.forEach(v => {
+        const module = ctx.server.moduleGraph.getModuleById(v)
+        module && ctx.server.reloadModule(module)
+      })
     },
 
     resolveId(id) {
-      if (
-        id === VIRTUAL_MODULE_ID_1 ||
-        id === VIRTUAL_MODULE_ID_2 ||
-        id === VIRTUAL_MODULE_ID_3 ||
-        id === VIRTUAL_MODULE_ID_4 ||
-        id === VIRTUAL_MODULE_ID_5
-      ) {
+      if (VIRTUAL_MODULE_IDS.includes(id)) {
         return id
       }
-      return
     },
 
     load(id) {
-      if (
-        id === VIRTUAL_MODULE_ID_1 ||
-        id === VIRTUAL_MODULE_ID_2 ||
-        id === VIRTUAL_MODULE_ID_3 ||
-        id === VIRTUAL_MODULE_ID_4 ||
-        id === VIRTUAL_MODULE_ID_5
-      ) {
+      if (VIRTUAL_MODULE_IDS.includes(id)) {
         return code[id]
       }
-      return
     },
 
     transform(code, id) {
-      if (/.abc$/.test(id)) {
-        return `
-          import module1 from "VIRTUAL_MODULE_ID_1"
-          import module2 from "VIRTUAL_MODULE_ID_2"
-          import module3 from "VIRTUAL_MODULE_ID_3"
-          import module4 from "VIRTUAL_MODULE_ID_4"
-          import module5 from "VIRTUAL_MODULE_ID_5"
-
-          module1()
-          module2()
-          module3()
-          module4()
-          module5()
-
-          export default "${code}"
-      `
+      if (/App\.vue$/.test(id)) {
+        return code.replace(
+          /(<script(.)+>)/,
+          `$1
+${VIRTUAL_MODULE_IDS.map((v, k) => `import module${k} from "${v}"`).join('\n')}
+${VIRTUAL_MODULE_IDS.map((v, k) => `module${k}()`).join('\n')}\n`
+        )
       }
-      return
     }
   }
 }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), ViteAbcPlugin()]
+  plugins: [ViteVuePrePlugin(), vue()]
 })
